1503246f2fbd2f96d266919e2fda02b54b23dd23
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-08 04:05:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,12 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 
 function removeElementFromBeginningOfArray (array) {
   return array.slice(1);
+}
   
 function destructivelyRemoveElementFromEndOfArray (array) {
   return array.pop();
 }
+
+function removeElementFromEndOfArray (array) {
+  return array.slice( 0, array.length - 1);
 }
