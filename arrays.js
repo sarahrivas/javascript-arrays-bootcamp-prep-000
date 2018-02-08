b61fa7580e16b5ad0d8f87256c8d0898b812173a
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-08 04:00:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,4 +24,6 @@ function accessElementInArray (array, index) {
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOf
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  
+}
